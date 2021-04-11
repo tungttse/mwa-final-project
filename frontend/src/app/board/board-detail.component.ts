c4297fa11f8a52b42b449de5833aa6ab3b94acd2
Thanh Tung Truong <tungttse@gmail.com>
2021-04-11 00:07:17 -0500

    [frontend] add some event to lisent change item

--- frontend/src/app/board/board-detail.component.ts
+++ frontend/src/app/board/board-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BoardService } from '../services/board.service'
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { DragulaService } from "ng2-dragula";
-
+import { Subscription } from "rxjs";
 @Component({
   selector: 'board-detail',
   templateUrl: 'board-detail.component.html',
@@ -13,50 +13,88 @@ export class BoardDetailComponent implements OnInit {
   content: String = "fetching..."
 
   //TODO: hardcode
-  public columns:Array<any> = [
+  public columns: Array<any> = [
     {
       name: 'Todo',
-      cards: [{name: 'Item A'}, {name: 'Item B'}, {name: 'Item C'}, {name: 'Item D'}]
+      cards: [{ name: 'Item A' }, { name: 'Item B' }, { name: 'Item C' }, { name: 'Item D' }]
     },
     {
       name: 'Doing',
-      cards: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
+      cards: [{ name: 'Item 1' }, { name: 'Item 2' }, { name: 'Item 3' }, { name: 'Item 4' }]
     },
 
     {
       name: 'Verifying',
-      cards: [{name: 'Item 1'}, {name: 'Item 2'}, {name: 'Item 3'}, {name: 'Item 4'}]
+      cards: [{ name: 'Item 1' }, { name: 'Item 2' }, { name: 'Item 3' }, { name: 'Item 4' }]
     },
 
     {
       name: 'Done',
-      cards: [{name: 'Item 1'}]
+      cards: [{ name: 'Item 1' }]
     },
   ];
 
+  subs = new Subscription();
   constructor(
     private boardService: BoardService,
     private dragulaService: DragulaService
-     ) {
+  ) {
 
-      this.dragulaService.createGroup("COLUMNS", {
-        direction: 'horizontal',
-        moves: (el, source, handle) => {
-          console.log(el, source)
-          return handle.className === "group-handle"
-        }
-      });
-
+    this.dragulaService.createGroup("COLUMNS", {
+      direction: 'horizontal',
+      moves: (el, source, handle) => {
+        return handle.className === "group-handle"
       }
+    });
+
+    this.subs.add(this.dragulaService.dropModel("COLUMNS")
+      .subscribe(({ el, target, source, sourceModel, targetModel, item }) => {
+        console.log('dropModel:', el, source, target, sourceModel, targetModel, item);
+      })
+    );
+    this.subs.add(this.dragulaService.removeModel("COLUMNS")
+      .subscribe(({ el, source, item, sourceModel }) => {
+        console.log('removeModel:');
+        console.log(el);
+        console.log(source);
+        console.log(sourceModel);
+        console.log(item);
+      })
+    );
+
+    this.subs.add(this.dragulaService.drag("COLUMNS")
+      .subscribe(({ name, el, source }) => {
+        console.log("drag ", name, el, source)
+      })
+    );
+    this.subs.add(this.dragulaService.drop("COLUMNS")
+      .subscribe(({ name, el, target, source, sibling }) => {
+        console.log("drop ", name, el,target,  source, sibling)
+      })
+    );
+    
+
+    // You can also get all events, not limited to a particular group
+    this.subs.add(this.dragulaService.drop()
+      .subscribe(({ name, el, target, source, sibling }) => {
+        console.log("drop catch all ", name, el,target,  source, sibling)
+      })
+    );
+
+
+  }
 
   ngOnInit(): void {
     this.boardService.get().subscribe(
       res => {
-        if(res['error']) {
+        if (res['error']) {
           this.content = "Error " + res['error']
         }
         this.content = res['data']
       }
     )
   }
+
+
+
 }
