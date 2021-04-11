3ccfdaface53be558042c7b44195c0594a513912
AhmadOsaili <ahmadosaili@hotmail.com>
2021-04-11 17:47:58 -0500

    Update boardRouters.js

--- backend/api/boardRouters.js
+++ backend/api/boardRouters.js
@@ -6,10 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('list boards');
 });
 
-router.post('/', async function(req, res){
-  await req.boardsCollection.insert(req.body);
-  res.json({"aaaaaaaaa" : " aaaa"})
-
-});
-
 module.exports = router;
