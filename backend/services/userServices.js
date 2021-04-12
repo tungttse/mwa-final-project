775a368f621c96ab6f75fd325af8095bc48731b2
Thanh Tung Truong <tungttse@gmail.com>
2021-04-11 22:06:35 -0500

    [backend] - format document

--- backend/services/userServices.js
+++ backend/services/userServices.js
@@ -1,61 +1,61 @@
 class UserServices {
-	constructor(collection){
-		this.dbCollection = collection
-	}
+  constructor(collection) {
+    this.dbCollection = collection
+  }
 
-	findByEmail(email){
-		return new Promise((resolve, reject) => {
-			try {
-				this.dbCollection.findOne({ _id: email })
-					.then(rs => {
-						if (rs) {
-							resolve(rs)
-						} else {
-							reject()
-						}
-					})
-					
-			} catch (e) {
-				reject(e) 
-			}
-		})
-	}
-	
-	insert(data) {
-		return new Promise((resolve, reject) => {
-			try {
-				this.dbCollection.findOne({_id: data._id})
-					.then(findResult => {
-						if(findResult) reject("exist")
-						else {
-							this.dbCollection.insertOne(data)
-							.then((insertResult) => {
-								if(insertResult.insertedCount) resolve() 
-								else {
-									reject(insertResult) 
-								}
-							})
-						}
-					})
-			} catch (e) {
-				reject(e) 
-			}
-		})
-	}
+  findByEmail(email) {
+    return new Promise((resolve, reject) => {
+      try {
+        this.dbCollection.findOne({ _id: email })
+          .then(rs => {
+            if (rs) {
+              resolve(rs)
+            } else {
+              reject()
+            }
+          })
 
-	checkEmail(email) {
-		return new Promise((resolve, reject) => {
-			try {
-				this.dbCollection.findOne({_id: email})
-					.then(findResult => {
-						if(findResult) resolve()
-						else reject() 
-					})
-			} catch (e) {
-				resolve() 
-			}
-		})
-	}
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
+
+  insert(data) {
+    return new Promise((resolve, reject) => {
+      try {
+        this.dbCollection.findOne({ _id: data._id })
+          .then(findResult => {
+            if (findResult) reject("exist")
+            else {
+              this.dbCollection.insertOne(data)
+                .then((insertResult) => {
+                  if (insertResult.insertedCount) resolve()
+                  else {
+                    reject(insertResult)
+                  }
+                })
+            }
+          })
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
+
+  checkEmail(email) {
+    return new Promise((resolve, reject) => {
+      try {
+        this.dbCollection.findOne({ _id: email })
+          .then(findResult => {
+            if (findResult) resolve()
+            else reject()
+          })
+      } catch (e) {
+        resolve()
+      }
+    })
+  }
 };
 
 module.exports = UserServices
