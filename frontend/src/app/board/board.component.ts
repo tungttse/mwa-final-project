3680e8d2311a077a8b96adb09e4312a5de7a03de
Thanh Tung Truong <tungttse@gmail.com>
2021-04-12 16:12:10 -0500

    [frontend] - add create new board input UI

--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -5,40 +5,8 @@ import { UserService } from '../services/user.service'
 
 @Component({
   selector: 'app-protected',
-  template: `
-  <div>
-    <div class="container content-area">
-        <br>
-        <br>
-        <div class="container content-grid">
-        <mat-grid-list cols="3" rowHeight="2:3">
-          <div *ngFor="let board of boards">
-            <mat-grid-tile><a routerLink='/boards/{{board._id}}'>{{board.name}}</a></mat-grid-tile>
-          </div>
-        </mat-grid-list>
-
-        </div>
-    </div>
-</div>
-`,
-  styles: [`
-  mat-grid-tile {
-    background: lightblue;
-    border : 1px solid rebeccapurple;
-    background-color: aquamarine;
-    cursor: pointer;
-    margin: 5px;
-  }
-  
-  .content-grid {
-      
-  }
-
-  .content-area{
-    margin: 0 auto;
-    max-width: 650px;
-  }
-  `]
+  templateUrl: 'board.component.html',
+  styleUrls: ['board.component.css']
 })
 export class BoardComponent implements OnInit {
   content: String = "fetching..."
@@ -56,5 +24,9 @@ export class BoardComponent implements OnInit {
         this.boards = res['boards']
       }
     )
+  }
+
+  createNewBoard(event) {
+    console.log('new boad', event.target.value)
   }
 }
